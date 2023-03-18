a9679cdb25f9dd0503436b7ee0d22431c4c415c3
vaibhavsingh-workstation <vaibhavsingh31390@gmail.com>
2023-03-18 16:25:58 +0530

    [INIT] Chat-App-Backend Initialized

---
+++ server.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config/config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+// DB CONNECT
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("DB Connected.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// IMPORT AND START APP
+const app = require("./index");
+app.listen(process.env.PORT, () => {
+  if (process.env.NODE_ENV == "dev") {
+    console.log(`App Running In Dev Mode on port ${process.env.PORT}`);
+  } else {
+    console.log(`App Running on port ${process.env.PORT}`);
+  }
+});
